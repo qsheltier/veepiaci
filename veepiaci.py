bc80e2077af96a026e4ef0f51b6cb1d68489b939
David Roden <github-a8in@qsheltier.de>
2024-12-01 11:45:11 +0100

    💄 Add main window

--- veepiaci.py
+++ veepiaci.py
@@ -1,17 +1,116 @@
 import sys
-import random
-from PySide6 import QtWidgets
+
+from PySide6 import QtCore, QtWidgets
+
+class VeepiaciSettings:
+    def __init__(self):
+        self.checksumFile = ""
+        self.directory = ""
+        self.resultFile = ""
 
 class VeepiaciMainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, settings: VeepiaciSettings):
         super().__init__()
+        self.settings = settings
         self.setWindowTitle("veepiaci")
+
+        central_window = QtWidgets.QWidget()
+        window_layout = QtWidgets.QGridLayout(central_window)
+        self.setCentralWidget(central_window)
+
+        self.checksumFileField = QtWidgets.QLineEdit()
+        self.checksumFileField.setReadOnly(True)
+        checksum_file_box = self.create_group_box(
+            "Checksum File",
+            "This contains the checksums for the files of a directory. Veepiaci can read files in the\nfollowing format: Veepiaci, UltraISO.",
+            self.checksumFileField,
+            "…",
+            self.choose_checksum_file
+        )
+        window_layout.addWidget(checksum_file_box, 0, 0)
+
+        self.directoryField = QtWidgets.QLineEdit()
+        self.directoryField.setReadOnly(True)
+        directory_box = self.create_group_box(
+            "Directory",
+            "This directory will have all of its files checked against the checksums contained in the\nchecksum file.",
+            self.directoryField,
+            "…",
+            self.choose_directory
+        )
+        window_layout.addWidget(directory_box, 1, 0)
+
+        self.verificationResultFileField = QtWidgets.QLineEdit()
+        self.verificationResultFileField.setReadOnly(True)
+        verification_result_file_box = self.create_group_box(
+            "Verification Result",
+            "The result of the verification run will be stored in this file.",
+            self.verificationResultFileField,
+            "…",
+            self.choose_result_file
+        )
+        window_layout.addWidget(verification_result_file_box, 2, 0)
+
+        window_layout.setRowStretch(3, 1)
+
+        start_verification_button = QtWidgets.QPushButton("Start Verification")
+
+        button_box = QtWidgets.QWidget()
+        button_box_layout = QtWidgets.QHBoxLayout(button_box)
+        button_box_layout.addStretch(1)
+        button_box_layout.addWidget(start_verification_button)
+        window_layout.addWidget(button_box, 4, 0)
+
+    @staticmethod
+    def create_group_box(title, description, field, button_text, on_click):
+        box = QtWidgets.QGroupBox(title)
+        label = QtWidgets.QLabel(description)
+        label.setWordWrap(False)
+        button = QtWidgets.QPushButton(button_text)
+        button.clicked.connect(on_click)
+        layout = QtWidgets.QGridLayout(box)
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(field, 1, 0)
+        layout.addWidget(button, 1, 1)
+        return box
+
+    @QtCore.Slot()
+    def choose_checksum_file(self):
+        (checksum_file, _) = QtWidgets.QFileDialog.getOpenFileName(self, "Select Checksum File")
+        if checksum_file is not None:
+            self.set_checksum_file(checksum_file)
+
+    def set_checksum_file(self, checksum_file):
+        self.settings.checksumFile = checksum_file
+        self.checksumFileField.setText(checksum_file)
+
+    @QtCore.Slot()
+    def choose_directory(self):
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self)
+        if directory is not None:
+            self.set_directory(directory)
+
+    def set_directory(self, directory):
+        self.settings.directory = directory
+        self.directoryField.setText(directory)
+
+    @QtCore.Slot()
+    def choose_result_file(self):
+        (result_file, _) = QtWidgets.QFileDialog.getSaveFileName(self, "Select Result File")
+        if result_file is not None:
+            self.set_result_file(result_file)
+
+    def set_result_file(self, result_file):
+        self.settings.resultFile = result_file
+        self.verificationResultFileField.setText(result_file)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = VeepiaciMainWindow()
-    widget.resize(800, 450)
+    settings = VeepiaciSettings()
+    widget = VeepiaciMainWindow(settings)
+    widget.resize(widget.layout().minimumSize())
+    widget.layout().setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetFixedSize)
     widget.show()
 
     sys.exit(app.exec())
