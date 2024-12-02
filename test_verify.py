ed7cc9d4fce4bbc43ec3b7b5777ed7151a6ea8c0
David Roden <github-a8in@qsheltier.de>
2024-12-02 16:58:58 +0100

    ♻️ Extract file creation into methods

--- test_verify.py
+++ test_verify.py
@@ -6,24 +6,30 @@ import verify
 from checksumfile import ChecksumFile
 
 
+def create_empty_file(temp_directory, filename="empty.dat"):
+    with open(os.path.join(temp_directory, filename), "wb"):
+        pass
+
+
+def create_file_with_data(temp_directory, filename="data.dat"):
+    with open(os.path.join(temp_directory, filename), "wb") as empty:
+        empty.write(bytearray(range(256)))
+
+
 class VerifyTest(unittest.TestCase):
 
     def test_verify_reports_success_when_all_files_match(self):
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_empty_file(temp_directory)
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}, "data.dat": {"md5": "e2c865db4162bed963bfaa9ef6ac18f0"}})
             verify_result = verify.verify_checksums(checksum_file, temp_directory)
             self.assertTrue(verify_result.success)
 
     def test_verify_reports_failure_when_a_file_does_not_match(self):
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_empty_file(temp_directory)
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427f"}, "data.dat": {"md5": "e2c865db4162bed963bfaa9ef6ac18f0"}})
             verify_result = verify.verify_checksums(checksum_file, temp_directory)
             self.assertFalse(verify_result.success)
@@ -33,8 +39,7 @@ class VerifyTest(unittest.TestCase):
 
     def test_verify_reports_failure_when_a_file_is_missing(self):
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
+            create_empty_file(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}, "data.dat": {"md5": "e2c865db4162bed963bfaa9ef6ac18f0"}})
             verify_result = verify.verify_checksums(checksum_file, temp_directory)
             self.assertFalse(verify_result.success)
@@ -44,10 +49,8 @@ class VerifyTest(unittest.TestCase):
 
     def test_verify_reports_failure_when_an_additional_file_is_present(self):
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_empty_file(temp_directory)
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}})
             verify_result = verify.verify_checksums(checksum_file, temp_directory)
             self.assertFalse(verify_result.success)
@@ -58,10 +61,8 @@ class VerifyTest(unittest.TestCase):
     def test_verify_calls_event_handler_for_every_file_it_hashes(self):
         hashed_files = []
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_empty_file(temp_directory)
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}, "data.dat": {"md5": "e2c865db4162bed963bfaa9ef6ac18f0"}})
             verify.verify_checksums(checksum_file, temp_directory, lambda f, h: hashed_files.append(f))
             self.assertCountEqual(hashed_files, ["data.dat", "empty.dat"])
@@ -69,10 +70,8 @@ class VerifyTest(unittest.TestCase):
     def test_verify_does_not_call_event_handler_when_a_file_does_not_have_a_hash(self):
         hashed_files = []
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "empty.dat"), "wb"):
-                pass
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_empty_file(temp_directory)
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}})
             verify.verify_checksums(checksum_file, temp_directory, lambda f, h: hashed_files.append(f))
             self.assertEqual(hashed_files, ["empty.dat"])
@@ -80,8 +79,7 @@ class VerifyTest(unittest.TestCase):
     def test_verify_does_not_call_event_handler_when_a_file_does_not_exist(self):
         hashed_files = []
         with tempfile.TemporaryDirectory() as temp_directory:
-            with open(os.path.join(temp_directory, "data.dat"), "wb") as empty:
-                empty.write(bytearray(range(256)))
+            create_file_with_data(temp_directory)
             checksum_file = ChecksumFile("test", {"empty.dat": {"md5": "d41d8cd98f00b204e9800998ecf8427e"}, "data.dat": {"md5": "e2c865db4162bed963bfaa9ef6ac18f0"}})
             verify.verify_checksums(checksum_file, temp_directory, lambda f, h: hashed_files.append(f))
             self.assertEqual(hashed_files, ["data.dat"])
